2be42d960ce4498f84a85ab5e9f059d202b86e05
Duncan P. N. Exon Smith <dexonsmith@apple.com>
2021-10-27 16:41:10 -0700

    Support: Add vfs::OutputFile::createProxy()
    Add OutputFile::createProxy(), which returns a raw_pwrite_stream that
    lets OutputFile know when it has been destroyed. Clients can hand a
    `std::unique_ptr<raw_pwrite_stream>` off to a producer and then call
    OutputFile::keep() or OutputFile::discard() after the producer is
    finished.
    
    Compared to using `buffer_unique_ostream`, a proxy has these advantages:
    
    - Flushes on the proxy show up immediately in the output.
    - Forwards colours correctly to terminal outputs.
    - Skips an unnecessary in-memory buffer.
    
    Compared to using `raw_pwrite_stream_proxy` directly,
    OutputFile::createProxy() has these advantages:
    
    - Checks for (and rejects) attempts to create two proxies concurrently.
    - Resets (and flushes) the open proxy in OutputFile::discard(), avoiding
      flush-after-close crashes and making write-after-close crashes more
      consistent.
    - Crashes if there's an open proxy in OutputFile::keep(), catching
      problems where the proxy lifetime is not correct.
    
    discard() is more lenient with open proxies than is keep() in order to
    simplify tearing down state after an error, since micromanaging
    destruction order might be challenging.

--- llvm/lib/Support/VirtualOutputFile.cpp
+++ llvm/lib/Support/VirtualOutputFile.cpp
@@ -9,17 +9,52 @@
 #include "llvm/Support/VirtualOutputFile.h"
 #include "llvm/Support/VirtualOutputError.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/raw_ostream_proxy.h"
 
 using namespace llvm;
 using namespace llvm::vfs;
 
 void OutputFileImpl::anchor() {}
 
+class OutputFile::TrackedProxy : public raw_pwrite_stream_proxy {
+public:
+  void resetProxy() {
+    TrackingPointer = nullptr;
+    resetProxiedOS();
+  }
+
+  explicit TrackedProxy(TrackedProxy *&TrackingPointer, raw_pwrite_stream &OS)
+      : raw_pwrite_stream_proxy(OS), TrackingPointer(TrackingPointer) {
+    assert(!TrackingPointer && "Expected to add a proxy");
+    TrackingPointer = this;
+  }
+
+  ~TrackedProxy() override { resetProxy(); }
+
+  TrackedProxy *&TrackingPointer;
+};
+
+Expected<std::unique_ptr<raw_pwrite_stream>> OutputFile::createProxy() {
+  if (OpenProxy)
+    return make_error<OutputError>(getPath(), OutputErrorCode::has_open_proxy);
+
+  return std::make_unique<TrackedProxy>(OpenProxy, getOS());
+}
+
 Error OutputFile::keep() {
   // Catch double-closing logic bugs.
   if (LLVM_UNLIKELY(!Impl))
     report_fatal_error(
         make_error<OutputError>(getPath(), OutputErrorCode::already_closed));
+
+  // Report a fatal error if there's an open proxy and the file is being kept.
+  // This is safer than relying on clients to remember to flush(). Also call
+  // OutputFile::discard() to give the backend a chance to clean up any
+  // side effects (such as temporaries).
+  if (LLVM_UNLIKELY(OpenProxy))
+    report_fatal_error(joinErrors(
+        make_error<OutputError>(getPath(), OutputErrorCode::has_open_proxy),
+        discard()));
 
   Error E = Impl->keep();
   Impl = nullptr;
@@ -32,6 +67,13 @@ Error OutputFile::discard() {
   if (LLVM_UNLIKELY(!Impl))
     report_fatal_error(
         make_error<OutputError>(getPath(), OutputErrorCode::already_closed));
+
+  // Be lenient about open proxies since client teardown paths won't
+  // necessarily clean up in the right order. Reset the proxy to flush any
+  // current content; if there is another write, there should be quick crash on
+  // null dereference.
+  if (OpenProxy)
+    OpenProxy->resetProxy();
 
   Error E = Impl->discard();
   Impl = nullptr;
