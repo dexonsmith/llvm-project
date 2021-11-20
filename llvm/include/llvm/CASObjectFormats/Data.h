ba85dc0d3f8af5952f2a191061fd41858bf66c46
Duncan P. N. Exon Smith <dexonsmith@apple.com>
2021-11-20 01:09:22 -0800

    CAS.o: Add comprehensive encodings for symbol attributes
    There are five axes:
    
    - Scope: the scope of the linkage. (local? exported? undefined?)
        - Exported symbols may be "hidden" (not in dynamic symbol table),
          "global" (in dynamic symbol table), or "protected" (special ELF
          feature).
        - Undefined symbols can be "undef" or "undef_or_null", the latter
          matching `extern_weak` (null if the symbol is not found).
    - Two axes only apply to global/protected/hidden symbols:
        - Hiding: whether it's semantically valid to hide/drop from the
          symbol table at any time (downgrading the scope from global to
          hidden and/or from hidden to local). "hideable" vs. "default".
        - Linkage: whether duplicate definitions should merge by name
          "weak" vs. "default".
    - Two axes apply to all EXCEPT undefined symbols.
        - KeepAlive: whether the symbol is semantically required to stay
          alive. Symbols are always required to live when they are
          referenced; usually they must also be kept when exported.
          "default" (also keep when exported) vs. "referenced" (only keep
          when referenced) vs. "always" (like `llvm.used`).
        - UnnamedAddr: whether the symbol's address is known not to be
          taken. "default" vs. "local" (if not taken in the compile unit
          it's defined in) vs. "global" (if guaranteed not to be taken).

--- llvm/include/llvm/CASObjectFormats/Data.h
+++ llvm/include/llvm/CASObjectFormats/Data.h
@@ -32,6 +32,342 @@ jitlink::MemProt decodeProtectionFlags(SectionProtectionFlags Perms);
 
 SectionProtectionFlags encodeProtectionFlags(jitlink::MemProt Perms);
 
+/// The scope of the variable.
+enum class Scope {
+  /// Not exported. Only visibile within a compile unit.
+  ///
+  /// LLVM IR: "default" visibility; "internal" or "private".
+  Local,
+
+  /// Exported from the compile unit, but not the linkage unit. Can be
+  /// referenced from other compile units.
+  ///
+  /// The link effectively downgrades to \a Local by not including this in the
+  /// dynamic symbol table.
+  ///
+  /// LLVM IR: "hidden" visibility.
+  Hidden,
+
+  /// Exported from the linkage unit. Can be referenced from other linkage
+  /// units.
+  ///
+  /// LLVM IR: "default" visibility; not "internal" or "private".
+  /// "linkonce_odr".
+  Global,
+
+  /// Exported from the linkage unit. Can be referenced from other linkage
+  /// units. Cannot be replaced by other linkage units.
+  ///
+  /// LLVM IR: "protected" visibility.
+  Protected,
+
+  /// Undefined / externally defined.
+  ///
+  /// LLVM IR: "external" linkage.
+  Undefined,
+
+  /// Undefined / externally defined, or null.
+  ///
+  /// LLVM IR: "extern_weak" linkage.
+  UndefinedOrNull,
+};
+raw_ostream &operator<<(raw_ostream &OS, const Scope &S);
+
+enum class Hiding {
+  /// Should stay in the symbol table unless dead-stripped.
+  Default,
+
+  /// Can be removed from the symbol table without changing program semantics.
+  ///
+  /// LLVM IR: "linkonce_odr" with "unnamed_addr" or when constant with
+  /// "local_unnamed_addr".
+  Hideable,
+};
+raw_ostream &operator<<(raw_ostream &OS, const Hiding &H);
+
+/// How to link together duplicate symbols. Not relevant for \a Scope::Local.
+enum class Linkage {
+  /// A clashing name is a duplicate definition error; or, \a Scope::Local.
+  ///
+  /// LLVM IR: "internal", "private", and "external".
+  Default,
+
+  /// Weak linkage: one definition is selected at link time. Definitions
+  /// are guaranteed to be semantically equivalent.
+  ///
+  /// LLVM IR: "common", "weak", "weak_odr", "linkonce", and "linkonce_odr".
+  Weak,
+};
+raw_ostream &operator<<(raw_ostream &OS, const Linkage &L);
+
+/// Rules for whether a symbol is discardable.
+enum class KeepAlive {
+  /// Discardable if not referenced and not exported.
+  ///
+  /// LLVM IR: most symbols not in "!llvm.used".
+  Default,
+
+  /// Discardable if even if exported.
+  ///
+  /// LLVM IR: "linkonce" and "linkonce_odr" unless in "!llvm.used".
+  /// "available_externally" also matches this semantically, but it'll never be
+  /// emitted anyway.
+  Referenced,
+
+  /// Always keep alive / never discardable.
+  ///
+  /// LLVM IR: symbols in "!llvm.used".
+  Always,
+
+  /// Convenience for data validation.
+  Max = Always,
+};
+raw_ostream &operator<<(raw_ostream &OS, const KeepAlive &K);
+
+/// Semantic guarantees about the relevance of a symbol's identity, which
+/// affects whether symbols can start aliasing each other (by merging their
+/// blocks).
+///
+/// See: \a llvm::GlobalValue::UnnamedAddr.
+enum class UnnamedAddress {
+  /// No guarantees. Address may be relevant.
+  ///
+  /// LLVM IR: most symbols.
+  Default,
+
+  /// Address is not taken locally. It's possible that another compile unit or
+  /// linkage unit takes its address.
+  ///
+  /// If/when the scope becomes \a Scope::Local (e.g., after hiding from symbol
+  /// table), can become an alias of another symbol that is known to have the
+  /// same content.
+  ///
+  /// LLVM IR: "local_unnamed_addr".
+  Local,
+
+  /// Address is not taken. Either not exported (\a Scope::Local), or there is
+  /// a semantic guarantee that the address is not relevant.
+  ///
+  /// Can become an alias of another symbol that is known to have the same
+  /// content.
+  ///
+  /// LLVM IR: "unnamed_addr".
+  Global,
+
+  /// Convenience for data validation.
+  Max = Global,
+};
+raw_ostream &operator<<(raw_ostream &OS, const UnnamedAddress &U);
+
+/// Symbol attributes.
+///
+/// Valid combinations depend on the \a Scope. Setters (that assert on invalid)
+/// provided for other attributes.
+///
+/// Encoding guaranteed to be a single byte. In-memory bitfield takes two bytes
+/// (for simplicity).
+class SymbolAttributes {
+public:
+  constexpr Scope getScope() const { return Scope(S); }
+  constexpr Hiding getHiding() const { return Hiding(H); }
+  constexpr Linkage getLinkage() const { return Linkage(L); }
+  constexpr KeepAlive getKeepAlive() const { return KeepAlive(K); }
+  constexpr UnnamedAddress getUnnamedAddress() const {
+    return UnnamedAddress(U);
+  }
+
+  void setHiding(Hiding NewH) {
+    assert((NewH == Hiding::Default || isExported()) &&
+           "Only exported symbols can be hideable");
+    H = unsigned(NewH);
+  }
+
+  void setLinkage(Linkage NewL) {
+    assert((NewL == Linkage::Default || isExported()) &&
+           "Only exported symbols can be weak");
+    L = unsigned(NewL);
+  }
+
+  void setKeepAlive(KeepAlive NewK) {
+    assert((NewK == KeepAlive::Default || !isUndefined()) &&
+           "Cannot keep alive undefined symbol alive");
+    K = unsigned(NewK);
+  }
+
+  void setUnnamedAddress(UnnamedAddress NewU) {
+    assert((NewU == UnnamedAddress::Default || !isUndefined()) &&
+           "Cannot mark undefined symbol as address-not-taken");
+    U = unsigned(NewU);
+    assert(isValid());
+  }
+
+  /// Set the attributes implied by ".weak_def_can_be_hidden".
+  void setAutoHide() {
+    assert(isExported() && "Can only auto-hide exported symbols");
+    setLinkage(Linkage::Weak);
+    setHiding(Hiding::Hideable);
+    if (getKeepAlive() == KeepAlive::Default)
+      setKeepAlive(KeepAlive::Referenced);
+    if (getUnnamedAddress() == UnnamedAddress::Default)
+      setUnnamedAddress(UnnamedAddress::Local);
+  }
+
+  /// Is this symbol undefined / imported / defined elsewhere?
+  constexpr bool isUndefined() const {
+    return getScope() == Scope::Undefined ||
+           getScope() == Scope::UndefinedOrNull;
+  }
+
+  /// Is this symbol defined locally and exported for linking?
+  constexpr bool isExported() const {
+    return getScope() == Scope::Global || getScope() == Scope::Hidden ||
+           getScope() == Scope::Protected;
+  }
+
+  /// Is this symbol defined locally and not exported?
+  constexpr bool isLocal() const { return getScope() == Scope::Local; }
+
+  /// Is this symbol always considered "used" even if not referenced?
+  bool isUsed() const { return getKeepAlive() == KeepAlive::Always; }
+
+  /// Is this symbol weak?
+  bool isWeak() const { return getLinkage() == Linkage::Weak; }
+
+  /// Should this symbol be treated like a symbol declared with the Mach-O
+  /// ".weak_def_can_be_hidden" directive?
+  bool isAutoHide() const {
+    return getHiding() == Hiding::Hideable && getLinkage() == Linkage::Weak &&
+           getUnnamedAddress() >= UnnamedAddress::Local;
+  }
+
+  /// Will this symbol be hidden from the symbol table (local) after linking?
+  bool isHiddenAfterLink() const { return getScope() <= Scope::Hidden; }
+
+  /// Can this symbol be hidden from the symbol table (made local) after
+  /// linking?
+  bool isHideableAfterLink() const {
+    return isHiddenAfterLink() || getHiding() == Hiding::Hideable;
+  }
+
+  /// Can the symbol be discarded if not referenced?
+  bool canDiscard() const {
+    if (isUsed())
+      return false;
+    return !isExported() || getKeepAlive() == KeepAlive::Referenced;
+  }
+
+  /// Can the symbol be discarded after linking if no longer (or still not)
+  /// referenced?
+  bool canDiscardAfterLink() const {
+    if (isUsed())
+      return false;
+    return canDiscard() || isHideableAfterLink();
+  }
+
+  /// If it points at a constant, is this safe to merge with other constants?
+  bool canMergeConstants() const {
+    return getUnnamedAddress() >= UnnamedAddress::Local;
+  }
+
+  /// Provide a stable sort order.
+  int compare(const SymbolAttributes &RHS) const {
+#define LLVM_DATA_SYMBOL_ATTRIBUTES_COMPARE_ATTRIBUTE(X)                       \
+  if (X != RHS.X)                                                              \
+    return X < RHS.X ? -1 : 1;
+    LLVM_DATA_SYMBOL_ATTRIBUTES_COMPARE_ATTRIBUTE(S)
+    LLVM_DATA_SYMBOL_ATTRIBUTES_COMPARE_ATTRIBUTE(H)
+    LLVM_DATA_SYMBOL_ATTRIBUTES_COMPARE_ATTRIBUTE(L)
+    LLVM_DATA_SYMBOL_ATTRIBUTES_COMPARE_ATTRIBUTE(K)
+    LLVM_DATA_SYMBOL_ATTRIBUTES_COMPARE_ATTRIBUTE(U)
+#undef LLVM_DATA_SYMBOL_ATTRIBUTES_COMPARE_ATTRIBUTE
+    return 0;
+  }
+  bool operator==(const SymbolAttributes &RHS) const {
+    return compare(RHS) == 0;
+  }
+  bool operator!=(const SymbolAttributes &RHS) const {
+    return compare(RHS) != 0;
+  }
+  bool operator<(const SymbolAttributes &RHS) const { return compare(RHS) < 0; }
+  bool operator>(const SymbolAttributes &RHS) const { return compare(RHS) > 0; }
+  bool operator<=(const SymbolAttributes &RHS) const {
+    return compare(RHS) <= 0;
+  }
+  bool operator>=(const SymbolAttributes &RHS) const {
+    return compare(RHS) >= 0;
+  }
+
+  void print(raw_ostream &OS) const;
+  void dump() const;
+  friend raw_ostream &operator<<(raw_ostream &OS, const SymbolAttributes &SA) {
+    SA.print(OS);
+    return OS;
+  }
+
+  /// Guarantee the size of the attributes.
+  static constexpr size_t EncodedSize = 1;
+  void encode(SmallVectorImpl<char> &Data) const;
+  static Expected<SymbolAttributes> consume(StringRef &Data);
+  static Expected<SymbolAttributes> decode(StringRef Data);
+
+  static constexpr SymbolAttributes getUndefined() {
+    return SymbolAttributes(Scope::Undefined);
+  }
+
+  static constexpr SymbolAttributes getUndefinedOrNull() {
+    return SymbolAttributes(Scope::UndefinedOrNull);
+  }
+
+  static constexpr SymbolAttributes
+  getLocal(KeepAlive K = KeepAlive::Default,
+           UnnamedAddress U = UnnamedAddress::Default) {
+    return SymbolAttributes(Scope::Local, Hiding::Default, Linkage::Default, K,
+                            U);
+  }
+  static constexpr SymbolAttributes getLocal(UnnamedAddress U) {
+    return SymbolAttributes(Scope::Local, Hiding::Default, Linkage::Default,
+                            KeepAlive::Default, U);
+  }
+
+  static SymbolAttributes get(const jitlink::Symbol &Symbol);
+
+  explicit constexpr SymbolAttributes(
+      Scope S = Scope::Local, Hiding H = Hiding::Default,
+      Linkage L = Linkage::Default, KeepAlive K = KeepAlive::Default,
+      UnnamedAddress U = UnnamedAddress::Default)
+      : S(unsigned(S)), H(unsigned(H)), L(unsigned(L)), K(unsigned(K)),
+        U(unsigned(U)) {
+    assert(isValid() && "Expected valid symbol attributes");
+  }
+  explicit constexpr SymbolAttributes(
+      Scope S, Linkage L, KeepAlive K = KeepAlive::Default,
+      UnnamedAddress U = UnnamedAddress::Default)
+      : SymbolAttributes(S, Hiding::Default, L, K, U) {}
+
+private:
+  constexpr bool isValid() const {
+    // Most attributes are not applicable to undefined symbols.
+    if (isUndefined())
+      return getHiding() == Hiding::Default &&
+             getLinkage() == Linkage::Default &&
+             getKeepAlive() == KeepAlive::Default &&
+             getUnnamedAddress() == UnnamedAddress::Default;
+
+    // Local symbols have no linkage.
+    if (isLocal())
+      return getHiding() == Hiding::Default && getLinkage() == Linkage::Default;
+
+    // Confirm the scope is valid.
+    return isExported();
+  }
+
+  unsigned S : 3;
+  unsigned H : 1;
+  unsigned L : 1;
+  unsigned K : 2;
+  unsigned U : 2;
+};
+
 /// The kind and offset of a fixup (e.g., for a relocation).
 struct Fixup {
   jitlink::Edge::Kind Kind;
--- llvm/lib/CASObjectFormats/Data.cpp
+++ llvm/lib/CASObjectFormats/Data.cpp
@@ -29,6 +29,196 @@ SectionProtectionFlags data::encodeProtectionFlags(jitlink::MemProt Perms) {
       ((Perms & jitlink::MemProt::Exec) != jitlink::MemProt::None ? Exec : 0));
 }
 
+constexpr size_t SymbolAttributes::EncodedSize;
+
+SymbolAttributes SymbolAttributes::get(const jitlink::Symbol &Symbol) {
+  Optional<data::SymbolAttributes> Attrs;
+  if (Symbol.isExternal()) {
+    Attrs = Symbol.getLinkage() == jitlink::Linkage::Weak
+                ? data::SymbolAttributes::getUndefinedOrNull()
+                : data::SymbolAttributes::getUndefined();
+  } else {
+    Optional<data::Scope> S;
+    switch (Symbol.getScope()) {
+    case jitlink::Scope::Default:
+      S = data::Scope::Global;
+      break;
+    case jitlink::Scope::Hidden:
+      S = data::Scope::Hidden;
+      break;
+    case jitlink::Scope::Local:
+      S = data::Scope::Local;
+      break;
+    }
+    Attrs.emplace(*S);
+
+    // FIXME: Investigate whether it's valid for LinkGraph::addCommonSymbol()
+    // to be called with jitlink::Scope::Local; more likely, this
+    // "isExported()" check can be skipped and the test in
+    // NestedV1SchemaTest::BlockSymbols should be changed instead (and maybe an
+    // assertion added to LinkGraph?).
+    if (Attrs->isExported())
+      if (Symbol.getLinkage() == jitlink::Linkage::Weak)
+        Attrs->setLinkage(Linkage::Weak);
+  }
+
+  if (Symbol.isLive())
+    Attrs->setKeepAlive(KeepAlive::Always);
+
+  // Fine-tune various settings for Mach-O's ".weak_def_can_be_hidden".
+  if (Symbol.isAutoHide())
+    Attrs->setAutoHide();
+
+  return *Attrs;
+}
+
+void SymbolAttributes::print(raw_ostream &OS) const {
+  OS << getScope();
+  if (isUndefined())
+    return;
+
+  auto printAttr = [&](StringRef Prefix, auto V) {
+    if (V != decltype(V)::Default)
+      OS << '+' << Prefix << V;
+  };
+  printAttr("", getHiding());
+  printAttr("", getLinkage());
+  printAttr("keep=", getKeepAlive());
+  printAttr("noaddr=", getUnnamedAddress());
+}
+
+LLVM_DUMP_METHOD void SymbolAttributes::dump() const { print(dbgs()); }
+
+namespace {
+/// Pack into 1B.
+///
+/// Note the following unused bitspace:
+///
+/// - Scope  == Undefined|UndefinedOrNull => other fields irrelevant.
+/// - Scope  == Local                     => Linkage/Hiding irrelevant.
+/// - Hiding == Hideable                  => Scope is Global/Protected/Hidden.
+/// - UnnamedAddress                      => three cases.
+/// - KeepAlive                           => three cases.
+///
+/// Taking advantage of just a few of those allows this encoding:
+///
+/// "00001100" => Scope=Undefined
+/// "00001101" => Scope=UndefinedOrNull
+/// "hlssuukk" => Scope=Local/Hidden/Global/Protected
+///
+/// Where "uu" / UnnamedAddress of "11" is reserved for undefined symbols.
+enum SymbolAttributesBits : unsigned {
+  // Bit locations.
+  SA_KeepAliveStart = 0,
+  SA_UnnamedAddressStart = 2,
+  SA_ScopeStart = 4,
+  SA_LinkageStart = 6,
+  SA_HidingStart = 7,
+
+  // Masks for fields.
+  SA_KeepAliveMask = 0x3u << SA_KeepAliveStart,
+  SA_UnnamedAddressMask = 0x3u << SA_UnnamedAddressStart,
+  SA_ScopeMask = 0x3u << SA_ScopeStart,
+  SA_LinkageMask = 0x1u << SA_LinkageStart,
+  SA_HidingMask = 0x1u << SA_HidingStart,
+
+  // Special bits for undefined symbols. SA_IsUndefinedBits avoids conflicting
+  // with valid values from the other fields.
+  SA_IsUndefinedBits = SA_UnnamedAddressMask,
+  SA_UndefinedValue = SA_IsUndefinedBits | 0x0,
+  SA_UndefinedOrNullValue = SA_IsUndefinedBits | 0x1,
+};
+using SymbolAttributesEncodedT = uint8_t;
+static_assert(sizeof(SymbolAttributesEncodedT) == SymbolAttributes::EncodedSize,
+              "Ensure EncodedSize is in sync with SymbolAttributesEncodedT.");
+} // end namespace
+
+static SymbolAttributesEncodedT
+encodeSymbolAttributes(const SymbolAttributes &SA) {
+  if (SA.getScope() == Scope::Undefined)
+    return SA_UndefinedValue;
+  if (SA.getScope() == Scope::UndefinedOrNull)
+    return SA_UndefinedOrNullValue;
+  assert(!SA.isUndefined() && "Expected defined symbol");
+
+  unsigned Byte = unsigned(SA.getKeepAlive()) << SA_KeepAliveStart |
+                  unsigned(SA.getUnnamedAddress()) << SA_UnnamedAddressStart |
+                  unsigned(SA.getScope()) << SA_ScopeStart |
+                  unsigned(SA.getLinkage()) << SA_LinkageStart |
+                  unsigned(SA.getHiding()) << SA_HidingStart;
+  assert((Byte & SA_IsUndefinedBits) != SA_IsUndefinedBits &&
+         "Expected to reserve IsUndefined bits");
+  assert(Byte <= UINT8_MAX && "Expected 1B of data");
+  return Byte;
+}
+
+void SymbolAttributes::encode(SmallVectorImpl<char> &Data) const {
+  static_assert(EncodedSize == 1, "Ensure push_back will work.");
+  Data.push_back(encodeSymbolAttributes(*this));
+}
+
+static Expected<SymbolAttributes>
+decodeSymbolAttributes(SymbolAttributesEncodedT Data) {
+  if (Data == SA_UndefinedValue)
+    return SymbolAttributes::getUndefined();
+  if (Data == SA_UndefinedOrNullValue)
+    return SymbolAttributes::getUndefinedOrNull();
+
+  auto makeError = [Data]() {
+    return createStringError(inconvertibleErrorCode(),
+                             "invalid symbol attributes '" +
+                                 Twine((unsigned long long)Data) + "'");
+  };
+
+  // Check if bits reserved for undefined attributes are corrupt.
+  if ((Data & SA_IsUndefinedBits) == SA_IsUndefinedBits)
+    return makeError();
+
+  auto extract = [Data](SymbolAttributesEncodedT Mask, int Start,
+                        unsigned Max) {
+    unsigned Extracted = (Data & Mask) >> Start;
+
+    // Check logic for masks and shifting. This should pass even if there is
+    // data corruption.
+    assert(Extracted < Max && "Logic error in encoding");
+    return Extracted;
+  };
+#define SA_EXTRACT_MACRO(T, M) T(extract(SA_##T##Mask, SA_##T##Start, M))
+  auto K = SA_EXTRACT_MACRO(KeepAlive, 4);
+  auto U = SA_EXTRACT_MACRO(UnnamedAddress, 4);
+  auto S = SA_EXTRACT_MACRO(Scope, 4);
+  auto L = SA_EXTRACT_MACRO(Linkage, 2);
+  auto H = SA_EXTRACT_MACRO(Hiding, 2);
+#undef SA_EXTRACT_MACRO
+
+  // Check for corrupt data before constructing.
+  if (U > UnnamedAddress::Max || K > KeepAlive::Max)
+    return makeError();
+  if (S == Scope::Local && !(L == Linkage::Default && H == Hiding::Default))
+    return makeError();
+
+  return SymbolAttributes(S, H, L, K, U);
+}
+
+Expected<SymbolAttributes> SymbolAttributes::decode(StringRef Data) {
+  if (Data.size() != EncodedSize)
+    return createStringError(inconvertibleErrorCode(),
+                             "invalid symbol attributes");
+  return decodeSymbolAttributes(Data[0]);
+}
+
+Expected<SymbolAttributes> SymbolAttributes::consume(StringRef &Data) {
+  if (Data.empty())
+    return createStringError(inconvertibleErrorCode(),
+                             "invalid symbol attributes");
+
+  SymbolAttributes SA;
+  if (Error E = decodeSymbolAttributes(Data[0]).moveInto(SA))
+    return E;
+  Data = Data.drop_front();
+  return SA;
+}
+
 void FixupList::encode(ArrayRef<Fixup> Fixups, SmallVectorImpl<char> &Data) {
   // FIXME: Kinds should be numbered in a stable way, not just rely on
   // Edge::Kind.
@@ -249,3 +439,54 @@ void TargetInfoList::iterator::decode(bool IsInit) {
   TI->Addend = Addend;
   TI->Index = IndexAndHasAddend >> 1;
 }
+
+#define CASE_SA_PRINT_ENUM(V, S)                                               \
+  V:                                                                           \
+  OS << S;                                                                     \
+  break
+raw_ostream &data::operator<<(raw_ostream &OS, const Scope &S) {
+  switch (S) {
+  case CASE_SA_PRINT_ENUM(Scope::Undefined, "undef");
+  case CASE_SA_PRINT_ENUM(Scope::UndefinedOrNull, "undef_or_null");
+  case CASE_SA_PRINT_ENUM(Scope::Local, "local");
+  case CASE_SA_PRINT_ENUM(Scope::Global, "global");
+  case CASE_SA_PRINT_ENUM(Scope::Protected, "protected");
+  case CASE_SA_PRINT_ENUM(Scope::Hidden, "hidden");
+  }
+  return OS;
+}
+
+raw_ostream &data::operator<<(raw_ostream &OS, const Hiding &H) {
+  switch (H) {
+  case CASE_SA_PRINT_ENUM(Hiding::Default, "default");
+  case CASE_SA_PRINT_ENUM(Hiding::Hideable, "hideable");
+  }
+  return OS;
+}
+
+raw_ostream &data::operator<<(raw_ostream &OS, const Linkage &L) {
+  switch (L) {
+  case CASE_SA_PRINT_ENUM(Linkage::Default, "default");
+  case CASE_SA_PRINT_ENUM(Linkage::Weak, "weak");
+  }
+  return OS;
+}
+
+raw_ostream &data::operator<<(raw_ostream &OS, const KeepAlive &K) {
+  switch (K) {
+  case CASE_SA_PRINT_ENUM(KeepAlive::Default, "none");
+  case CASE_SA_PRINT_ENUM(KeepAlive::Referenced, "referenced");
+  case CASE_SA_PRINT_ENUM(KeepAlive::Always, "always");
+  }
+  return OS;
+}
+
+raw_ostream &data::operator<<(raw_ostream &OS, const UnnamedAddress &U) {
+  switch (U) {
+  case CASE_SA_PRINT_ENUM(UnnamedAddress::Default, "default");
+  case CASE_SA_PRINT_ENUM(UnnamedAddress::Local, "local");
+  case CASE_SA_PRINT_ENUM(UnnamedAddress::Global, "global");
+  }
+  return OS;
+}
+#undef CASE_SA_PRINT_ENUM
